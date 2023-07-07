e5e510e87d0f34eb68d63873ec9a4ed7f18d0fc8
Ilia Almaev <shtrihcode@outlook.com>
2023-07-07 13:46:14 +0300

    add

--- mdadm/script_raid.sh
+++ mdadm/script_raid.sh
@@ -3,4 +3,4 @@ mdadm --create --verbose /dev/md1 -l 6 -n 5 /dev/sd{b,c,d,e,f}
 mkdir /etc/mdadm
 touch /etc/mdadm/mdadm.conf
 echo "DEVICE partitions" > /etc/mdadm/mdadm.conf 
-mdadm --detail --scan --verbose | awk '/ARRAY/{print}' >> /etc/mdadm/mdadm.conf 
+mdadm --detail --scan --verbose | awk '/ARRAY/{print}' >> /etc/mdadm/mdadm.conf
