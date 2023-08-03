715bce6774ce74b22ab7484aae80e6d74c8df266
Ilia Almaev <shtrihcode@outlook.com>
2023-08-03 14:23:13 +0300

    repo

--- rpm/repo.sh
+++ rpm/repo.sh
@@ -7,56 +7,20 @@ cp /root/rpmbuild/RPMS/x86_64/nginx-1.24.0-1.el8.ngx.x86_64.rpm /usr/share/nginx
 wget https://downloads.percona.com/downloads/percona-distribution-mysql-ps/percona-distribution-mysql-ps-8.0.28/binary/redhat/8/x86_64/percona-orchestrator-3.2.6-2.el8.x86_64.rpm -O /usr/share/nginx/html/repo/percona-orchestrator-3.2.6-2.el8.x86_64.rpm
 # Create Repository
 createrepo /usr/share/nginx/html/repo/
-# Edit nginx default.conf
-[root@centos ~]# vi /etc/nginx/conf.d/default.conf
-# Add autoindex on
-location / {
-        root   /usr/share/nginx/html;
-        index  index.html index.htm;
-        autoindex on;
-    }
-# Check config
-[root@centos ~]# nginx -t
-nginx: the configuration file /etc/nginx/nginx.conf syntax is ok
-nginx: configuration file /etc/nginx/nginx.conf test is successful
+# Download nginx default.conf
+wget https://github.com/shtrihcode/otus/blob/main/rpm/default.conf
+# Copy and replace nginx default.conf
+cp -n default.conf /etc/nginx/conf.d/default.conf
 # Reload nginx settings
-[root@centos ~]# nginx -s reload
-# Check repo 
-[root@centos ~]# curl -a http://localhost/repo/
-<html>
-<head><title>Index of /repo/</title></head>
-<body>
-<h1>Index of /repo/</h1><hr><pre><a href="../">../</a>
-<a href="repodata/">repodata/</a>                                          03-Aug-2023 09:23                   -
-<a href="nginx-1.24.0-1.el8.ngx.x86_64.rpm">nginx-1.24.0-1.el8.ngx.x86_64.rpm</a>                  03-Aug-2023 09:19             2265136
-<a href="percona-orchestrator-3.2.6-2.el8.x86_64.rpm">percona-orchestrator-3.2.6-2.el8.x86_64.rpm</a>        16-Feb-2022 15:57             5222976
-</pre><hr></body>
-</html>
-
+nginx -s reload
 # Add new repo
 [root@centos ~]# cat >> /etc/yum.repos.d/otus.repo << EOF
-> 
-> [otus]
-> 
-> name=otus-linux
-> 
-> baseurl=http://localhost/repo
-> 
-> gpgcheck=0
-> 
-> enabled=1
-> 
-> EOF
-
-# Check repo add
-[root@centos ~]# yum repolist enabled | grep otus
-Failed to set locale, defaulting to C.UTF-8
-otus                otus-linux
-
-# List packages from repo
-[root@centos ~]# yum list | grep otus
-Failed to set locale, defaulting to C.UTF-8
-percona-orchestrator.x86_64                                       2:3.2.6-2.el8                                          otus
+[otus]
+name=otus-linux
+baseurl=http://localhost/repo
+gpgcheck=0
+enabled=1
+EOF
 # Install Percona-Server from repo
-[root@centos ~]# yum install percona-orchestrator.x86_64 -y
+yum install -y percona-orchestrator.x86_64
 
