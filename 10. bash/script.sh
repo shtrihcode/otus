d8532cfa9f902640e962908b54bcec44851d6d04
Ilia Almaev <shtrihcode@outlook.com>
2023-08-21 20:04:41 +0300

    10. BASH

--- 10. bash/script.sh
+++ 10. bash/script.sh
@@ -18,7 +18,7 @@ then
     ip=$(awk "NR>$lines"  ./access.log | awk '{print $1}' | sort | uniq -c | sort -rn | awk '{ if ( $1 >= 0 ) { print "Количество запросов:" $1, "IP:" $2 } }')
     addresses=$(awk '($9 ~ /200/)' ./access.log |awk '{print $7}'|sort|uniq -c|sort -rn|awk '{ if ( $1 >= 10 ) { print "Количество запросов:" $1, "URL:" $2 } }')
     # Ошибки c момента последнего запуска
-    errors=$(cat access.log | cut -d '"' -f3 | cut -d ' ' -f2 | sort | uniq -c | sort -rn)
+    errors=$(cat ./access.log | cut -d '"' -f3 | cut -d ' ' -f2 | sort | uniq -c | sort -rn)
     # Формирование сообщения и отправка почты
     echo -e "Данные за период:$timestart-$timeend\n$ip\n\n"Часто запрашиваемые адреса:"\n$addresses\n\n"Частые ошибки:"\n$errors" > mail1 #| mail -s "access log" almadm@localhost
 else
