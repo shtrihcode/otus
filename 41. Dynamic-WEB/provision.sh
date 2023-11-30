833683a6449ffb9edc46f30e178f0a4e415931a4
Ilia Almaev <shtrihcode@outlook.com>
2023-11-30 16:55:08 +0300

    41

--- 41. Dynamic-WEB/provision.sh
+++ 41. Dynamic-WEB/provision.sh
@@ -1,3 +1,9 @@
+yum install epel-release -y
+yum install docker vim -y
+yum install docker-ce docker-ce-cli containerd.io docker-compose -y
+
+systemctl start docker
+
 cp -R /vagrant/dockerlab/ /home/vagrant/
 
 cd /vagrant/dockerlab/
